9da3de9b12a5dd2c245371adff8a067a61ca859c
clkent <chelsea.kent@gmail.com>
2016-02-12 13:18:09 -0800

    env

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -5,8 +5,8 @@ class TwilioController < ApplicationController
 	def send_sms
 		message = params[:message]
     number = params[:number]
-    account_sid = 'AC8eda95c14c5d1268b1612899934c1bae'
-    auth_token = '3c071bb7c8aba72790ea9568ca57d60a'
+    account_sid = ENV["TWILIO_ACCOUNT_SID"] 
+    auth_token = ENV["TWILIO_AUTH_TOKEN"]
 
     @client = Twilio::REST::Client.new account_sid, auth_token
 
