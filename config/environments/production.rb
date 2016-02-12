cbb997533a8556759631a5dcc5572486218dfc3d
clkent <chelsea.kent@gmail.com>
2016-02-11 16:00:58 -0800

    try production change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-config.middleware.use Rack::TwilioWebhookAuthentication, Rails.application.secrets.twilio_auth_token, '/send_sms', '/send_sms_all'
+config.middleware.use Rack::TwilioWebhookAuthentication, Rails.application.secrets.twilio_auth_token, '/send_sms'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
